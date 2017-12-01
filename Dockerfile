ae66c9576918c7204a4ef1093e8f4ff52b31d0a8
Samar <sagrawal@noon.com>
2017-12-01 20:25:01 +0530

    switch back to flask

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM samar/alpine-sanic-aiomysql
+FROM samar/alpine-python3-flask
 MAINTAINER sagrawal@noon.com
 
 WORKDIR /src
 EXPOSE 5199
 
 CMD ["python3", "-m", "zyr.app"]
-
-RUN pip3 install hirlite==0.3.1 requests flask
 
 COPY . /src
 RUN cd /src && python3 setup.py develop
--- zyr/app.py
+++ zyr/app.py
@@ -1,57 +1,35 @@
-#from flask import Flask, jsonify, request, Response, stream_with_context
+from flask import Flask, jsonify, request, Response, stream_with_context
 import os
 import logging
 import requests
-from sanic import Sanic
-from sanic.response import json, redirect, html, stream
 
-#app = Flask(__name__)
-app = Sanic()
+app = Flask(__name__)
 
 hosttorequest = os.environ['HOST']
 
-# logging.basicConfig(
-#     format='%(asctime)-15s %(levelname)s %(message)s',
-#     level = logging.INFO
-#     )
-#
-# app.logger.handlers = []
+logging.basicConfig(
+    format='%(asctime)-15s %(levelname)s %(message)s',
+    level = logging.INFO
+    )
 
+app.logger.handlers = []
 
 @app.route('/')
-async def root(request):
-    req = requests.get('https://'+hosttorequest+'/', stream = True)
-    async def sample_streaming_fn(response):
-        response.write(await req.iter_content())
-    print(req.headers['content-type'])
-    return stream(sample_streaming_fn,content_type = req.headers['content-type'])
-
-    #return stream(req.iter_content(), content_type = req.headers['content-type'])
-    #assert req.status_code == 200, req['error']
-    #return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+def root():
+    req = requests.get('http://'+hosttorequest+'/', stream = True)
+    assert req.status_code == 200, req['error']
+    return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 
 @app.route('/<path:other>')
-async def other(request, other):
-    req = requests.get('https://'+hosttorequest+'/'+other, stream = True)
-    async def sample_streaming_fn(response):
-        print(req.iter_content())
-        yield response.write(req.iter_content())
+def other(other):
+    req = requests.get('http://'+hosttorequest+'/'+other, stream = True)
+    assert req.status_code == 200, req['error']
+    return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 
-    return stream(sample_streaming_fn,content_type = req.headers['content-type'])
-    #return stream(req.iter_content(), content_type = req.headers['content-type'])
-    #assert req.status_code == 200, req['error']
-    #return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
-
-# @app.after_request
-# def log_request(response):
-#     app.logger.info("[%s] %s %s" % (response.status_code, request.method, request.url))
-#     return response
-#
-# @app.errorhandler(AssertionError)
-# def handle_assertion(error):
-#     response = jsonify(error=error.message)
-#     response.status_code = 400
-#     return response
+@app.after_request
+def log_request(response):
+    app.logger.info("[%s] %s %s" % (response.status_code, request.method, request.url))
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5199, debug = True)
